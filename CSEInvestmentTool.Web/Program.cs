f09893f5257e2b11dda44e01c6d924df34972060
yazhodha <yazhodha@gmail.com>
2025-02-15 17:14:21 +0530

    Update program.cs

--- CSEInvestmentTool.Web/Program.cs
+++ CSEInvestmentTool.Web/Program.cs
@@ -1,13 +1,29 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using CSEInvestmentTool.Web.Data;
+using CSEInvestmentTool.Application.Services;
+using CSEInvestmentTool.Infrastructure.Data;
+using CSEInvestmentTool.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddControllers();
+
+// Add HttpClient
+builder.Services.AddHttpClient();
+
+// Register services
+builder.Services.AddScoped<IDataCollectionService, CSEDataCollectionService>();
+builder.Services.AddScoped<StockScoringService>();
+builder.Services.AddScoped<InvestmentAllocationService>();
+
+// Add PostgreSQL DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        npgsqlOptions => npgsqlOptions.EnableRetryOnFailure()
+    ));
 
 var app = builder.Build();
 
@@ -15,17 +31,23 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+app.MapControllers();
+
+// Initialize/migrate database
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
